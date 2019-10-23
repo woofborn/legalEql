6380d165db52001965bb469dd3a311919b030a98
“woofborn” <wolf.born@gmail.com>
2019-10-23 19:54:51 +0800

    Each project under a partner now has a link to that project page

--- views/partner.jsx
+++ views/partner.jsx
@@ -14,7 +14,7 @@ class Partner extends React.Component {
     if (this.props.projectList.length>0){
 
         projects = this.props.projectList.map((project,i)=>{
-             return <li key = {i}>{project.project_name}</li>
+             return <li key = {i}><a href = {`/projects/${project.project_name}`}>{project.project_name}</a></li>
              })
     }
 
